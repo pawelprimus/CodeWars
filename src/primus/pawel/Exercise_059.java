1997a9fbc6cf7940366d56cdcc9cfd47ad9d72ba
Pawel P <pawelprimus@gmail.com>
2021-04-07 23:34:21 +0200

    Added exercise 059 - Range extension

---
+++ src/primus/pawel/Exercise_059.java
@@ -0,0 +1,17 @@
+package primus.pawel;
+// Range extension
+// 4kyu
+// https://www.codewars.com/kata/51ba717bb08c1cd60f00002f/train/java
+// 2021-04-07 T:23:33:46
+class Exercise_059 {
+
+    public static String rangeExtraction(int[] arr) {
+
+        return "";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(rangeExtraction(new int[]{-6,-3,-2,-1,0,1,3,4,5,7,8,9,10,11,14,15,17,18,19,20}));
+    }
+
+}
