4806eee6810e60ec70f2b83e843be561da61baf3
Pawel P <pawelprimus@gmail.com>
2021-04-09 17:29:12 +0200

    Refactored exercise 62, still TODO

--- src/primus/pawel/Exercise_062.java
+++ src/primus/pawel/Exercise_062.java
@@ -1,23 +1,23 @@
 package primus.pawel;
 
-import java.util.stream.IntStream;
-
 // Is a number prime
 // 6kyu
 // https://www.codewars.com/kata/5262119038c0985a5b00029f/train/java
 // 2021-04-08 T:21:01:53
-// Time Out todo 
+// Time Out todo
 class Exercise_062 {
 
     public static boolean isPrime(int num) {
-        if(num<=1) return false;
-        for (int i = 2; i <= num / 2; i++) {
-            if(num%i == 0) return false;
+        if (num <= 1) return false;
+        if (num == 2) return true;
+        if (num % 2 == 0) return false;
+        for (int i = 3; i <= num / 2; i += 2) {
+            if (num % i == 0) return false;
         }
         return true;
     }
 
     public static void main(String[] args) {
-        System.out.println(isPrime(4));
+        System.out.println(isPrime(3));
     }
 }
