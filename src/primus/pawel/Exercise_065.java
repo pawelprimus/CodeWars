56f45a95d44edffe39f2c4c186db9d169d5e31c4
Pawel P <pawelprimus@gmail.com>
2021-04-11 20:36:47 +0200

    Added exercise 65 - String repeat

---
+++ src/primus/pawel/Exercise_065.java
@@ -0,0 +1,16 @@
+package primus.pawel;
+
+import org.junit.platform.commons.util.StringUtils;
+
+import java.util.Collections;
+
+// String repeat
+// 8kyu
+// https://www.codewars.com/kata/57a0e5c372292dd76d000d7e/train/java
+// 2021-04-11 T:20:32:33
+class Exercise_065 {
+
+    public static String repeatStr(final int repeat, final String string) {
+        return String.join("", Collections.nCopies(repeat, string));
+    }
+}
