a8982fa5c428d3e8f474b2f8438cdb5b26671f03
Pawel P <pawelprimus@gmail.com>
2021-04-11 20:29:30 +0200

    Finished exercise 62

--- src/primus/pawel/Exercise_062.java
+++ src/primus/pawel/Exercise_062.java
@@ -4,14 +4,13 @@ package primus.pawel;
 // 6kyu
 // https://www.codewars.com/kata/5262119038c0985a5b00029f/train/java
 // 2021-04-08 T:21:01:53
-// Time Out todo
 class Exercise_062 {
 
     public static boolean isPrime(int num) {
-        if (num <= 1) return false;
+        if (num <= 2) return false;
         if (num == 2) return true;
         if (num % 2 == 0) return false;
-        for (int i = 3; i <= num / 2; i += 2) {
+        for (int i = 3; i <= Math.sqrt(num); i += 2) {
             if (num % i == 0) return false;
         }
         return true;
