6f3ded14de5036627b8e752e17a0bc5af9d9283c
Pawel P <pawelprimus@gmail.com>
2021-04-19 23:21:55 +0200

    Added try catch to constructor v2

--- src/primus/pawel/Exercise_084.java
+++ src/primus/pawel/Exercise_084.java
@@ -4,21 +4,27 @@ package primus.pawel;
 // 7kyu
 // https://www.codewars.com/kata/55b75fcf67e558d3750000a3/train/java
 // 2021-04-19 T:22:47:42
+
+
 class Exercise_084 {
+
 
     public static class Block {
 
-        private  int width;
-        private  int length;
-        private  int height;
+        private int width;
+        private int length;
+        private int height;
 
         Block(int... dimensions) {
             try {
                 this.width = dimensions[0];
                 this.length = dimensions[1];
                 this.height = dimensions[2];
-            } catch (ArrayIndexOutOfBoundsException exception) {
-                System.out.println(exception);
+            } catch (ArrayIndexOutOfBoundsException e) {
+                throw e;
+                // e.printStackTrace();
+            } finally {
+                System.out.println("Something wrong");
             }
         }
 
@@ -48,9 +54,15 @@ class Exercise_084 {
 
     public static void main(String[] args) {
 
-        Block block = new Block(new int[]{1, 2});
+        try {
+            Block block = new Block(new int[]{1, 2});
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("aaa");
+        } finally {
+            System.out.println("bb");
+        }
 
-        System.out.println(block.getVolume());
+       // System.out.println(block.getVolume());
     }
 
 
