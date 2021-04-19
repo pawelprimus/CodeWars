e9bda10a94b853a0e205ce0fb84c91f2234b2fda
Pawel P <pawelprimus@gmail.com>
2021-04-19 23:11:22 +0200

    Added try catch to constructor

--- src/primus/pawel/Exercise_084.java
+++ src/primus/pawel/Exercise_084.java
@@ -1,45 +1,56 @@
 package primus.pawel;
+
 // Building blocks
 // 7kyu
 // https://www.codewars.com/kata/55b75fcf67e558d3750000a3/train/java
 // 2021-04-19 T:22:47:42
 class Exercise_084 {
 
-    public class Block{
+    public static class Block {
 
-        private final int width;
-        private final int length;
-        private final int height;
+        private  int width;
+        private  int length;
+        private  int height;
 
         Block(int... dimensions) {
-            this.width = dimensions[0];
-            this.length = dimensions[1];
-            this.height = dimensions[2];
+            try {
+                this.width = dimensions[0];
+                this.length = dimensions[1];
+                this.height = dimensions[2];
+            } catch (ArrayIndexOutOfBoundsException exception) {
+                System.out.println(exception);
+            }
         }
 
-        public int getWidth(){
+        public int getWidth() {
             return width;
         }
 
-        public int getLength(){
+        public int getLength() {
             return length;
         }
-        public int getHeight(){
+
+        public int getHeight() {
             return height;
         }
 
-        public int getVolume(){
-            return getWidth()*getLength()*getHeight();
+        public int getVolume() {
+
+            return getWidth() * getLength() * getHeight();
+
         }
 
-        public int getSurfaceArea(){
-            return 2*getWidth()*getLength() +  2*getLength()*getHeight() + 2*getWidth()*getHeight();
+        public int getSurfaceArea() {
+            return 2 * getWidth() * getLength() + 2 * getLength() * getHeight() + 2 * getWidth() * getHeight();
         }
 
     }
 
     public static void main(String[] args) {
 
+        Block block = new Block(new int[]{1, 2});
+
+        System.out.println(block.getVolume());
     }
 
 
