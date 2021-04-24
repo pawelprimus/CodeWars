29ddd1218e6058b6b82ae1390feebe23b745b174
Pawel P <pawelprimus@gmail.com>
2021-04-24 16:05:02 +0200

    Added exercise 96, check if array is in ascending order

---
+++ src/primus/pawel/Exercise_096.java
@@ -0,0 +1,15 @@
+package primus.pawel;
+// Are the numbers in order?
+// 7kyu
+// https://www.codewars.com/kata/56b7f2f3f18876033f000307/train/java
+// 2021-04-24 T:16:00:59
+class Exercise_096 {
+    public static boolean isAscOrder(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            if(arr[i] <  arr[i-1]) return false;
+        }
+        return true;
+    }
+
+
+}
