fdcb7eec0a3d1d031d159abc284288ecadbb803b
Pawel P <pawelprimus@gmail.com>
2021-04-26 23:42:19 +0200

    Added exercise 99 to do

---
+++ src/primus/pawel/Exercise_099.java
@@ -0,0 +1,12 @@
+package primus.pawel;
+
+// Fold an array
+// 7kyu
+// https://www.codewars.com/kata/57ea70aa5500adfe8a000110/train/java
+// 2021-04-26 T:23:41:49
+class Exercise_099 {
+    public static int[] foldArray(int[] array, int runs)
+    {
+        return new int[] { 0 };
+    }
+}
