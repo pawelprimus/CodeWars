b58a768f43e8604aebc35d63715b75e6a5131cae
Pawel P <pawelprimus@gmail.com>
2021-05-03 21:12:15 +0200

    Exercise 105 in work

--- src/primus/pawel/Exercise_105_todo.java
+++ src/primus/pawel/Exercise_105_todo.java
@@ -11,8 +11,50 @@ class Exercise_105_todo {
     public static boolean isAValidMessage(String message) {
         if (message.length() == 0) return true;
         if (Character.isLetter(message.charAt(0))) return false;
-        int num = Character.getNumericValue(message.charAt(0));
-        LinkedList<String> wordsList = new LinkedList<>();
+        StringBuilder sb = new StringBuilder();
+
+        LinkedList<String> numberList = new LinkedList<>();
+        LinkedList<String> wordList = new LinkedList<>();
+
+        for (int i = 0; i < message.length()-1; i++) {
+
+            while (Character.isDigit(message.charAt(i + 1)) && i < message.length() - 1) {
+                i++;
+                sb.append(message.charAt(i));
+            }
+            numberList.add(sb.toString());
+            sb.delete(0, sb.length());
+
+
+            while (Character.isLetter(message.charAt(i + 1)) && i < message.length() - 1) {
+                i++;
+                sb.append(message.charAt(i));
+            }
+            wordList.add(sb.toString());
+            sb.delete(0, sb.length());
+
+
+            if (i == message.length() - 1) {
+                if (Character.isLetter((message.charAt(i)))) {
+                    wordList.add(String.valueOf(message.charAt(i)));
+                } else {
+                    numberList.add(String.valueOf(message.charAt(i)));
+                }
+                i++;
+            }
+        }
+
+
+        for (
+                String word : numberList) {
+            System.out.println(word);
+        }
+
+
+        for (
+                String word : wordList) {
+            System.out.println(word);
+        }
 
         return true;
     }
