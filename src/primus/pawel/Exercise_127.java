c662623a931c2aa308afbea9185b61177696388c
Pawel P <pawelprimus@gmail.com>
2021-05-16 21:12:33 +0200

    Added exercise 127

---
+++ src/primus/pawel/Exercise_127.java
@@ -0,0 +1,25 @@
+package primus.pawel;
+
+import java.util.Arrays;
+
+// Averages of numbers
+// 7kyu
+// https://www.codewars.com/kata/57d2807295497e652b000139/train/java
+// 2021-05-16 T:20:59:41
+class Exercise_127 {
+
+    public static double[] averages(int[] numbers) {
+
+        if(numbers == null || numbers.length <= 1) return new double[0];
+
+        double[] results = new double[numbers.length - 1];
+        for (int i = 0; i < numbers.length - 1; i++) {
+            results[i] = (double) (numbers[i] + numbers[i + 1]) / 2;
+        }
+        return results;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
