15650d86859f609d94805184e420f4ba0d40c5c2
Pawel P <pawelprimus@gmail.com>
2021-05-22 22:38:42 +0200

    Ref exercise 96

--- src/primus/pawel/Exercise_096.java
+++ src/primus/pawel/Exercise_096.java
@@ -1,4 +1,5 @@
 package primus.pawel;
+
 // Are the numbers in order?
 // 7kyu
 // https://www.codewars.com/kata/56b7f2f3f18876033f000307/train/java
@@ -6,7 +7,7 @@ package primus.pawel;
 class Exercise_096 {
     public static boolean isAscOrder(int[] arr) {
         for (int i = 1; i < arr.length; i++) {
-            if(arr[i] <  arr[i-1]) return false;
+            if (arr[i] < arr[i - 1]) return false;
         }
         return true;
     }
