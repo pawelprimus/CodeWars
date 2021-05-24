e946e7f4c6d8de7cf1aa1d8948d65687623d966a
Pawel P <pawelprimus@gmail.com>
2021-05-25 01:30:01 +0200

    Finished exercise 136

--- src/primus/pawel/Exercise_136.java
+++ src/primus/pawel/Exercise_136.java
@@ -1,7 +1,28 @@
 package primus.pawel;
-//
-//
-//
-//
+
+// Make a Window
+// 6kyu
+// https://www.codewars.com/kata/59c03f175fb13337df00002e/train/java
+// 2021-05-25 T:01:12:00
 class Exercise_136 {
+    public static String makeAWindow(int num) {
+        StringBuilder result = new StringBuilder();
+        result.append("-".repeat(num * 2 + 3)).append("\n");
+
+        result.append((("|" + ".".repeat(num)) + "|" + ".".repeat(num) + "|" + "\n").repeat(num));
+
+        result.append("|").append("-".repeat(num)).append("+").append("-".repeat(num)).append("|").append("\n");
+
+        result.append((("|" + ".".repeat(num)) + "|" + ".".repeat(num) + "|" + "\n").repeat(num));
+
+        result.append("-".repeat(num * 2 + 3));
+       // System.out.println(result.toString());
+
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        makeAWindow(3);
+    }
+
 }
