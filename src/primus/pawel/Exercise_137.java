ca78fa9fe0aef5685cf59090c0469e7b56849811
Pawel P <pawelprimus@gmail.com>
2021-05-26 21:01:16 +0200

    Exercsie 137 todo

---
+++ src/primus/pawel/Exercise_137.java
@@ -0,0 +1,22 @@
+package primus.pawel;
+
+//
+// 7 kyu
+// https://www.codewars.com/kata/54557d61126a00423b000a45/train/java
+// 2021-05-26 T:20:53:29
+class Exercise_137 {
+
+    public static String shorterReverseLonger(String a, String b) {
+        StringBuilder rev = new StringBuilder();
+        StringBuilder result = new StringBuilder();
+        if (a.length() > b.length()) {
+            rev.append(a).reverse();
+            result.append(b).append(rev.toString()).append(b);
+        } else {
+            rev.append(b).reverse();
+            result.append(a).append(rev.toString()).append(a);
+        }
+        return result.toString();
+    }
+
+}
