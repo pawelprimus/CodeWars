fc1d83a4f308fa12c3b1b82a91bef8660bff1e8f
Pawel P <pawelprimus@gmail.com>
2021-06-07 23:22:06 +0200

    Exercise 150 todo

--- src/primus/pawel/Exercise_150_todo.java
+++ src/primus/pawel/Exercise_150_todo.java
@@ -3,7 +3,7 @@ package primus.pawel;
 // 6kyu
 // https://www.codewars.com/kata/5a9c35e9ba1bb5c54a0001ac/train/java
 // 2021-06-06 T:14:15:01
-class Exercise_150 {
+class Exercise_150_todo {
 
     public static int add(int x, int y)
     {
