2f6e764d2917ccc4dea826069edced474989d601
Pawel P <pawelprimus@gmail.com>
2021-06-13 23:51:05 +0200

    Exercise 156 and 157 to do

---
+++ src/primus/pawel/Exercise_156.java
@@ -0,0 +1,11 @@
+package primus.pawel;
+// Phone Screen Organizer
+// 6kyu
+// https://www.codewars.com/kata/5e10ae1cdf8a12001da16779/train/java
+// 2021-06-12 T:16:24:41
+class Exercise_156 {
+    public static Object[] moveOver(Object[] a)
+    {
+        return null;
+    }
+}
