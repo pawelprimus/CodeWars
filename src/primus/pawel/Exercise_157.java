95d721311ab9c0c784a1fffc86f21b6ab1ca5bad
Pawel P <pawelprimus@gmail.com>
2021-06-14 23:28:27 +0200

    Exercise 157 todo

--- src/primus/pawel/Exercise_157.java
+++ src/primus/pawel/Exercise_157.java
@@ -1,7 +1,7 @@
 package primus.pawel;
-//
-//
-//
-//
+// Simple Fun #176: Reverse Letter
+// 7kyu
+// https://www.codewars.com/kata/58b8c94b7df3f116eb00005b/train/java
+// 2021-06-14 T:23:25:52
 class Exercise_157 {
 }
