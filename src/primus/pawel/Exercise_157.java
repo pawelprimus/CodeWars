6f65742aabd7c70f1a4eab8bca1c2a494fce320b
Pawel P <pawelprimus@gmail.com>
2021-06-15 00:00:27 +0200

    Finished exercise 157

--- src/primus/pawel/Exercise_157.java
+++ src/primus/pawel/Exercise_157.java
@@ -1,7 +1,32 @@
 package primus.pawel;
+
 // Simple Fun #176: Reverse Letter
 // 7kyu
 // https://www.codewars.com/kata/58b8c94b7df3f116eb00005b/train/java
 // 2021-06-14 T:23:25:52
 class Exercise_157 {
+
+    public static String reverseLetter(final String str) {
+        StringBuilder result = new StringBuilder();
+        for (int i = str.length() - 1; i >= 0; i--) {
+            if (Character.isLetter(str.charAt(i))) {
+                result.append(str.charAt(i));
+            }
+
+        }
+
+        return result.toString();
+    }
+
 }
+/* public static String reverseLetter(final String s) {
+        return new StringBuilder(s.replaceAll("[^a-zA-Z]", "")).reverse().toString();
+
+
+    }
+
+    public static String reverseLetter(final String str) {
+        return new StringBuilder(str.replaceAll("(?i)[^a-z]","")).reverse().toString();
+    }
+
+    */
