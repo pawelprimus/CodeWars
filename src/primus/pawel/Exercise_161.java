73ad10a846ff06fc99952f03e31644dcd17eb087
Pawel P <pawelprimus@gmail.com>
2021-06-24 22:43:14 +0200

    Finished exercise 162

--- src/primus/pawel/Exercise_161.java
+++ src/primus/pawel/Exercise_161.java
@@ -1,7 +1,12 @@
 package primus.pawel;
-//
-//
-//
-//
+// Remove String Spaces
+// 8kyu
+// https://www.codewars.com/kata/57eae20f5500ad98e50002c5/train/java
+// 2021-06-22 T:23:14:39
 class Exercise_161 {
+
+    static String noSpace(final String x) {
+        String result = x.replace(" ", "");
+        return result;
+    }
 }
---
+++ src/primus/pawel/Exercise_162.java
@@ -0,0 +1,32 @@
+package primus.pawel;
+
+import java.util.LinkedList;
+
+// What's in a name
+// 6kyu
+// 2021-06-24 T:22:21:40
+// https://www.codewars.com/kata/59daf400beec9780a9000045/train/java
+class Exercise_162 {
+
+    public static boolean nameInStr(String str, String name) {
+        LinkedList<Character> charsQue = new LinkedList();
+        for (int i = 0; i < name.length(); i++) {
+            charsQue.add(name.charAt(i));
+        }
+
+        int i = 0;
+        while (i < str.length() && charsQue.size() > 0) {
+            if (str.charAt(i) == charsQue.getFirst()) {
+                charsQue.remove();
+            }
+            i++;
+        }
+        return (charsQue.size() == 0);
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(nameInStr("cNext hto a lake", "chris"));
+
+    }
+}
