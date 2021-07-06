2a8759d1a265670af0c908723935d0a6de962aa9
Pawel P <pawelprimus@gmail.com>
2021-07-06 23:55:16 +0200

    Exercise 172

--- src/primus/pawel/Exercise_172.java
+++ src/primus/pawel/Exercise_172.java
@@ -1,7 +1,7 @@
 package primus.pawel;
-//
-//
-//
-//
+// Binary to Text (ASCII) Conversion
+// 6kyu
+// https://www.codewars.com/kata/5583d268479559400d000064/train/java
+// 2021-07-06 T:23:54:49
 class Exercise_172 {
 }
