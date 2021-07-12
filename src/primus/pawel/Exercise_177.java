57476be38e7ce2379d4ddd34e2fa781ca904421a
Pawel P <pawelprimus@gmail.com>
2021-07-12 23:54:47 +0200

    Finished exercise 177

--- src/primus/pawel/Exercise_177.java
+++ src/primus/pawel/Exercise_177.java
@@ -1,7 +1,24 @@
 package primus.pawel;
-//
-//
-// 
-//
+
+// Smallest value of an array
+// 7kyu
+// https://www.codewars.com/kata/544a54fd18b8e06d240005c0/train/java
+// 2021-07-12 T:23:48:46
 class Exercise_177 {
+
+    public static int findSmallest(final int[] numbers, final String toReturn) {
+        int smallestNum = Integer.MAX_VALUE;
+        int indexOfSmallestNUm = 0;
+
+        for (int i = 0; i < numbers.length; i++) {
+
+            if(numbers[i] < smallestNum){
+                smallestNum = numbers[i];
+                indexOfSmallestNUm = i;
+            }
+        }
+
+        return (toReturn.equals("value")) ? smallestNum : indexOfSmallestNUm;
+    }
+
 }
