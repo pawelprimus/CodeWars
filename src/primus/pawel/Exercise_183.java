173073777838e096a03ffd147541bcc5d4252647
Pawel P <pawelprimus@gmail.com>
2021-07-19 22:33:19 +0200

    Exercise 183 todo

---
+++ src/primus/pawel/Exercise_183.java
@@ -0,0 +1,19 @@
+package primus.pawel;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+//Password Maker
+// 6kyu
+// https://www.codewars.com/kata/5b3d5ad43da310743c000056
+//
+class Exercise_183 {
+
+
+    public static String makePassword(int len, boolean flagUpper, boolean flagLower, boolean flagDigit) {
+        // Your code here
+        return "Password123";
+    }
+
+
+}
