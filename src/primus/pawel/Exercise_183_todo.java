a57cad372460fe35d850b63f0876a047497a6f0f
Pawel P <pawelprimus@gmail.com>
2021-07-22 22:52:14 +0200

    exercise 183 todo

--- src/primus/pawel/Exercise_183_todo.java
+++ src/primus/pawel/Exercise_183_todo.java
@@ -7,7 +7,7 @@ import java.util.regex.Pattern;
 // 6kyu
 // https://www.codewars.com/kata/5b3d5ad43da310743c000056
 //
-class Exercise_183 {
+class Exercise_183_todo {
 
 
     public static String makePassword(int len, boolean flagUpper, boolean flagLower, boolean flagDigit) {
