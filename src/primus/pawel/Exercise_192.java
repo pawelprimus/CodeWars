409de645ea09568bd3fae0ce32bea8b37a1e6fec
Pawel P <pawelprimus@gmail.com>
2021-07-31 18:33:23 +0200

    Finished exercise 192

--- src/primus/pawel/Exercise_192.java
+++ src/primus/pawel/Exercise_192.java
@@ -1,7 +1,23 @@
 package primus.pawel;
-//
-//
-//
-//
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+// Responsible Drinking
+// 7kyu
+// https://www.codewars.com/kata/5aee86c5783bb432cd000018/train/java
+// 2021-07-31 T:18:23:15
 class Exercise_192 {
+    public static String hydrate(String drinkString) {
+        Pattern pattern = Pattern.compile("\\d");
+        Matcher matcher = pattern.matcher(drinkString);
+        int glassesOfWater = 0;
+        while (matcher.find()) {
+            glassesOfWater += Integer.parseInt(matcher.group());
+        }
+
+        return glassesOfWater<=1 ? glassesOfWater + " glass of water" : glassesOfWater + " glasses of water";
+    }
+
+
 }
