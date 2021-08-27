1384dd1306c31a5021214f379f519027819d028d
Pawel P <pawelprimus@gmail.com>
2021-08-27 14:29:46 +0200

    Finished exercise 203

---
+++ src/primus/pawel/Exercise_203.java
@@ -0,0 +1,31 @@
+package primus.pawel;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+// Valid Phone Number
+// 6kyu
+// https://www.codewars.com/kata/525f47c79f2f25a4db000025/train/java
+// 2021-08-27 T:14:15:03
+class Exercise_203 {
+
+    public static boolean validPhoneNumber(String phoneNumber) {
+        String regExp = ("\\((\\d{3})\\)"+ " " + "\\d{3}" + "-" + "\\d{4}");
+        return phoneNumber.matches(regExp);
+    }
+
+    public static void main(String[] args) {
+        validPhoneNumber("(003) 123");
+    }
+
+
+}
+
+/*^   - matches start of line
+\(  - matches the character '('
+\d  - matches any digit [0-9]
+{x} - matches the previous exactly x times
+\)  - matches the character ')'
+' ' - matches the character ' '
+'-' - matches the character '-'
+$   - matches end of line*/
