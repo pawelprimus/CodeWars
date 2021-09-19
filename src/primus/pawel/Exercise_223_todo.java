47602d379c3cb24c580ac955de7afb78dfa17f9b
Pawel P <pawelprimus@gmail.com>
2021-09-19 16:40:22 +0200

    Finished exercise 223

--- src/primus/pawel/Exercise_223_todo.java
+++ src/primus/pawel/Exercise_223_todo.java
@@ -1,31 +1,32 @@
 package primus.pawel;
 
-//
-//
+// Adding values of arrays in a shifted way
+// 7kyu
 // https://www.codewars.com/kata/57c7231c484cf9e6ac000090/train/java
-//
+// 2021-09-19 T:15:56:45
 class Exercise_223_todo {
 
     public static int[] addingShifted(int[][] arrayOfArrays, int shift) {
-        int fullLength = arrayOfArrays[0].length + shift;
+        int arraysAmount = arrayOfArrays.length;
+        int fullLength = arrayOfArrays[0].length + (arraysAmount - 1)*shift;
         int oneArrayLength = arrayOfArrays[0].length;
-        int[] resultArray = new int[oneArrayLength + shift];
+        int[] resultArray = new int[fullLength];
 
-        int currentNum = 0;
-        for (int i = 0; i < fullLength; i++) {
-            if (i < oneArrayLength) {
-                currentNum += arrayOfArrays[0][i];
+        for (int i = 0; i < arraysAmount; i++) {
+            for (int j = 0; j < oneArrayLength; j++) {
+                resultArray[j + i*shift] =+  arrayOfArrays[i][j];
             }
-
-            if (i >= shift) {
-                currentNum += arrayOfArrays[1][i - shift];
-            }
-
-            resultArray[i] = currentNum;
-            currentNum = 0;
         }
-
         return resultArray;
+    }
+
+
+    public static void main(String[] args) {
+
+        int[][] arrayOfArrays = new int[][]{new int[]{1, 2, 3, 4, 5, 6}, new int[]{7, 7, 7, -7, 7, 7}, new int[]{1, 1, 1, 1, 1, 1}};
+        System.out.println(addingShifted(arrayOfArrays, 3));
+
+
     }
 
 }
