72f66ec153afb96dca2b1c713b3fabe99375aaa9
Pawel P <pawelprimus@gmail.com>
2021-09-24 00:09:18 +0200

    Finished exercise 233

--- src/primus/pawel/Exercise_233.java
+++ src/primus/pawel/Exercise_233.java
@@ -1,7 +1,33 @@
 package primus.pawel;
+
 //
 //
 //
 // 2021-09-23 T:23:49:34
 class Exercise_233 {
+
+    public static int getWeight(String name) {
+        int result = 0;
+        char currentChar;
+        System.out.println(name);
+        for (int i = 0; i < name.length(); i++) {
+            currentChar = name.charAt(i);
+
+            if (Character.isLetter(currentChar)) {
+                if (Character.isUpperCase(currentChar)) {
+                    result += Character.toLowerCase(currentChar);
+                } else {
+                    result += Character.toUpperCase(currentChar);
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        getWeight("Joe ");
+
+    }
 }
