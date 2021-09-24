208e58f2cf2ab40bc37811721b4366f615b1f29b
Pawel P <pawelprimus@gmail.com>
2021-09-24 23:46:42 +0200

    Finished exercise 234

--- src/primus/pawel/Exercise_234.java
+++ src/primus/pawel/Exercise_234.java
@@ -1,4 +1,36 @@
 package primus.pawel;
 
+// Uglify Word
+// 7kyu
+// https://www.codewars.com/kata/5ce6cf94cb83dc0020da1929/train/java
+// 2021-09-24 T:23:38:26
 class Exercise_234 {
+    public static String uglifyWord(String str) {
+        boolean flag = true;
+        StringBuilder sb = new StringBuilder();
+
+        char currentChar;
+        for (int i = 0; i < str.length(); i++) {
+            currentChar = str.charAt(i);
+
+            if (Character.isLetter(currentChar)) {
+
+                if (flag) {
+                    flag = false;
+                    sb.append(Character.toUpperCase(currentChar));
+                } else {
+                    flag = true;
+                    sb.append(Character.toLowerCase(currentChar));
+                }
+
+            } else {
+                sb.append(currentChar);
+                flag = true;
+            }
+        }
+
+        return sb.toString();
+    }
+
+
 }
