c72e86e2217dc35445c44904ad8bd98133371210
Pawel P <pawelprimus@gmail.com>
2021-09-27 23:22:02 +0200

    Finished exercise 237

---
+++ src/primus/pawel/Exercise_237.java
@@ -0,0 +1,27 @@
+package primus.pawel;
+
+//
+//
+// https://www.codewars.com/kata/594b8e182fa0a0d7fc000875/train/java
+// 2021-09-27 T:23:14:34
+class Exercise_237 {
+
+    public static String findE(String str) {
+        if (str.length() == 0) {
+            return "";
+        }
+        if (str==null) {
+            return null;
+        }
+
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (Character.toLowerCase(str.charAt(i)) == 'e') {
+                count++;
+            }
+        }
+        return count > 0 ? String.valueOf(count) : "There is no \"e\".";
+    }
+
+
+}
--- src/primus/pawel/Main.java
+++ src/primus/pawel/Main.java
@@ -1,10 +1,12 @@
 package primus.pawel;
 
+import java.sql.Timestamp;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        int bitmask = 32;
+        // write your code here
+     /*   int bitmask = 32;
         int val = 1;
         char a = 'a';
         char A = 'A';
@@ -12,6 +14,9 @@ public class Main {
         System.out.println("Bitmask " + Integer.toBinaryString(bitmask));
         System.out.println( "val " +Integer.toBinaryString(val));
         System.out.println( "char a " +Integer.toBinaryString((a)));
-        System.out.println(A << 64);
+        System.out.println(A << 64);*/
+        Timestamp ts = new Timestamp(1632256605);
+        System.out.println(ts.toLocalDateTime());
+
     }
 }
