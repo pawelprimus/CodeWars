6893ee2d13069e88d747a881901234dfa46ff454
Pawel P <pawelprimus@gmail.com>
2021-09-29 20:38:27 +0200

    Finished exercise 238

--- src/primus/pawel/Exercise_238.java
+++ src/primus/pawel/Exercise_238.java
@@ -1,4 +1,14 @@
 package primus.pawel;
 
+import java.util.Collections;
+
+// Thinkful - String Drills: Repeate
+// 7kyu
+// https://www.codewars.com/kata/585a1a227cb58d8d740001c3/train/java
+// 2021-09-29 T:20:34:49
 class Exercise_238 {
+    public static String repeat(String string, long n) {
+        return String.join("", Collections.nCopies((int) n, string));
+    }
 }
+//return string.repeat((int)n);
