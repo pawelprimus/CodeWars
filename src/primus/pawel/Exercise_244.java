f34a8d2cd338e46a37889d659396e845bfe189d7
Pawel P <pawelprimus@gmail.com>
2021-10-09 12:52:41 +0200

    Finished exercise 244

--- src/primus/pawel/Exercise_244.java
+++ src/primus/pawel/Exercise_244.java
@@ -1,7 +1,12 @@
 package primus.pawel;
-//
-//
-//
-//
+
+// Maximum Multiple
+// 7kyu
+// https://www.codewars.com/kata/5aba780a6a176b029800041c/train/java
+// 2021-10-09 T:12:50:59
 class Exercise_244 {
+
+    public static int maxMultiple(int divisor, int bound) {
+        return bound - bound%divisor;
+    }
 }
