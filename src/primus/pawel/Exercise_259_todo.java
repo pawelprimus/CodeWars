3504e90ed0e60623f2888729eacdba5d2a674f9b
Pawel P <pawelprimus@gmail.com>
2021-10-25 23:38:21 +0200

    Exercise 261 todo

--- src/primus/pawel/Exercise_259_todo.java
+++ src/primus/pawel/Exercise_259_todo.java
@@ -1,4 +1,46 @@
 package primus.pawel;
 
+// Bits Battle
+// 7kyu
+// https://www.codewars.com/kata/58856a06760b85c4e6000055/train/java
+// 2021-10-24 T:20:35:47
 class Exercise_259_todo {
+
+
+    static String bitsBattle(int[] numbers) {
+
+        int odsPower = 0;
+        int evensPower = 0;
+
+        for (int i = 0; i < numbers.length; i++) {
+
+            if(numbers[i]%2 == 0 ){
+                evensPower += getPowerOfNumber(numbers[i]);
+            } {
+                odsPower += getPowerOfNumber(numbers[i]);
+            }
+
+        }
+
+        if (odsPower == evensPower) {
+            return "tie";
+        }
+        return odsPower > evensPower ? "odds win" : "evens win";
+    }
+
+    public static int getPowerOfNumber(int number) {
+
+        String binary = Integer.toBinaryString(number);
+        int ones = 0;
+        int zeros = 0;
+        for (int i = 0; i < binary.length(); i++) {
+
+            if (binary.charAt(i) == 48) {
+                zeros++;
+            } else {
+                ones++;
+            }
+        }
+        return number%2==0 ? zeros : ones;
+    }
 }
