8eb445c6c401b39af1d6b3b88153ed22eb3c5bd3
Pawel P <pawelprimus@gmail.com>
2021-10-31 11:45:37 +0100

    Finished exercise 263

--- src/primus/pawel/Exercise_263_todo.java
+++ src/primus/pawel/Exercise_263_todo.java
@@ -1,4 +1,31 @@
 package primus.pawel;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+// Flatten and sort an array
+// 7kyu
+// https://www.codewars.com/kata/57ee99a16c8df7b02d00045f/train/java
+// 2021-10-31 T:11:38:50
 class Exercise_263_todo {
+
+    public static int[] flattenAndSort(int[][] array) {
+
+        List<Integer> arrayList = new ArrayList<>();
+        for (int i = 0; i <array.length ; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                arrayList.add(array[i][j]);
+            }
+        }
+        int[] sortedArray = arrayList.stream().mapToInt(i -> i).toArray();
+        Arrays.sort(sortedArray);
+
+        return sortedArray;
+    }
+
+    public static void main(String[] args) {
+        flattenAndSort(new int[][] {{111, 999}, {222}, {333}, {444}, {888}, {777}, {666}, {555}});
+    }
 }
