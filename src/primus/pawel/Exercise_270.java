19caafb576e4bb6a68bc16a2e0ea76f93a5de85c
Pawel P <pawelprimus@gmail.com>
2021-11-06 10:58:02 +0100

    Finished exercise 270

--- src/primus/pawel/Exercise_270.java
+++ src/primus/pawel/Exercise_270.java
@@ -1,7 +1,14 @@
 package primus.pawel;
-//
-//
-//
-//
+// Greet Me
+// 7kyu
+// https://www.codewars.com/kata/535474308bb336c9980006f2/train/java
+// 2021-11-06 T:10:55:31
 class Exercise_270 {
+
+    public static String greet(String name){
+        StringBuilder result = new StringBuilder();
+        result.append("Hello ").append(Character.toUpperCase(name.charAt(0))).append(name.substring(1).toLowerCase()).append("!");
+        return result.toString();
+    }
+
 }
