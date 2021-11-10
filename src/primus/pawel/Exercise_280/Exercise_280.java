185780af2ebfbd5f2c8f05325e85e15a321ff851
Pawel P <pawelprimus@gmail.com>
2021-11-10 22:12:04 +0100

    Finished exercise 280

---
+++ src/primus/pawel/Exercise_280/Exercise_280.java
@@ -0,0 +1,14 @@
+package primus.pawel.Exercise_280;
+//
+//
+//
+//
+class Exercise_280 {
+
+    public static void main(String[] args) {
+        Dinglemouse dm = new Dinglemouse().setAge(2);
+       // Dinglemouse dm = new Dinglemouse();
+        System.out.println(dm.hello());
+    }
+}
+
