f5a8114772604d902211c558edc9a6621b459088
Pawel P <pawelprimus@gmail.com>
2021-11-13 13:25:19 +0100

    Finished exercise 283

--- src/primus/pawel/Exercise_283_todo.java
+++ src/primus/pawel/Exercise_283_todo.java
@@ -1,7 +1,33 @@
 package primus.pawel;
-//
-//
-//
-//
+
+import java.util.Arrays;
+
+// Product Of Maximums Of Array (Array Series #2)
+// 7kyu
+// https://www.codewars.com/kata/5a63948acadebff56f000018/train/java
+// 2021-11-13 T:13:08:54
 class Exercise_283_todo {
+
+    public static long maxProduct(int[] numbers, int sub_size) {
+        long sum = 1;
+        Arrays.sort(numbers);
+        for (int i = numbers.length - sub_size; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+            sum *= numbers[i];
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(maxProduct(new int[]{10, 3, -27, -1}, 3));
+    }
 }
+
+/*
+return stream(numbers)
+            .sorted()
+            .skip(numbers.length-subSize)
+            .mapToLong(Long::valueOf)
+            .reduce(1, (x,y)->x*y);
+
+            */
