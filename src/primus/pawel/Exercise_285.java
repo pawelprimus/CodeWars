0e8419ef02239702e4d0b82c8b33d7a0d07f58a0
Pawel P <pawelprimus@gmail.com>
2021-11-14 23:44:07 +0100

    Exercise 285 todo

---
+++ src/primus/pawel/Exercise_285.java
@@ -0,0 +1,14 @@
+package primus.pawel;
+// Execute me nTimes
+// 7kyu
+// https://www.codewars.com/kata/5b2b4836b6989d207700005b/train/java
+// 2021-11-14 T:23:42:52
+class Exercise_285 {
+
+    public void execute(Runnable action, int nTimes) {
+
+        for (int i =0; i<nTimes; i++){
+            action.run();
+        }
+    }
+}
