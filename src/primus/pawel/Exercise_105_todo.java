15182822616f1d9c7827a5cdb5b99861f25bd1f7
Pawel P <pawelprimus@gmail.com>
2021-11-22 23:01:44 +0100

    Finished exercise 105

--- src/primus/pawel/Exercise_105_todo.java
+++ src/primus/pawel/Exercise_105_todo.java
@@ -1,6 +1,6 @@
 package primus.pawel;
 
-import java.util.LinkedList;
+import java.util.Arrays;
 
 // Message validator
 // 6kyu
@@ -8,59 +8,41 @@ import java.util.LinkedList;
 // 2021-05-02 T:20:44:23
 class Exercise_105_todo {
 
-    public static boolean isAValidMessage(String message) {
-        if (message.length() == 0) return true;
-        if (Character.isLetter(message.charAt(0))) return false;
-        StringBuilder sb = new StringBuilder();
+    public static boolean isAValidMessage( String message) {
 
-        LinkedList<String> numberList = new LinkedList<>();
-        LinkedList<String> wordList = new LinkedList<>();
+        System.out.println(message);
 
-        for (int i = 0; i < message.length()-1; i++) {
-
-            while (Character.isDigit(message.charAt(i + 1)) && i < message.length() - 1) {
-                i++;
-                sb.append(message.charAt(i));
-            }
-            numberList.add(sb.toString());
-            sb.delete(0, sb.length());
-
-
-            while (Character.isLetter(message.charAt(i + 1)) && i < message.length() - 1) {
-                i++;
-                sb.append(message.charAt(i));
-            }
-            wordList.add(sb.toString());
-            sb.delete(0, sb.length());
-
-
-            if (i == message.length() - 1) {
-                if (Character.isLetter((message.charAt(i)))) {
-                    wordList.add(String.valueOf(message.charAt(i)));
-                } else {
-                    numberList.add(String.valueOf(message.charAt(i)));
-                }
-                i++;
-            }
+        if(message.length() == 0 || message == null) {
+            return true;
+        }
+        if(Character.isLetter(message.charAt(0))){
+            return false;
+        }
+        if(message.length() == 1 && message.charAt(0) == '0'){
+            return true;
         }
 
+        String numbers = message.replaceAll("[^0-9]", " ");
+        String words = message.replaceAll("\\d+", " ");
 
-        for (
-                String word : numberList) {
-            System.out.println(word);
+        String[] numberArray = numbers.trim().split("\\s+");
+        String[] wordsArray = words.trim().split("\\s+");
+
+        if(numberArray.length != wordsArray.length){
+            return false;
         }
 
-
-        for (
-                String word : wordList) {
-            System.out.println(word);
+        for (int i = 0; i < numberArray.length; i++) {
+            if(Integer.parseInt(numberArray[i]) != wordsArray[i].length() ){
+                return false;
+            }
         }
 
         return true;
     }
 
     public static void main(String[] args) {
-        isAValidMessage("4code13hellocodewars");
+        System.out.println(  isAValidMessage("0"));
 
     }
 
