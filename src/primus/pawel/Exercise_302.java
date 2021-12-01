9f53294ecfbba8ecc91ddfe0c22be611e79c67a6
Pawel P <pawelprimus@gmail.com>
2021-12-01 21:55:01 +0100

    Finished exercise 302

---
+++ src/primus/pawel/Exercise_302.java
@@ -0,0 +1,36 @@
+package primus.pawel;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+// Ordering the words!
+// 7kyu
+// https://www.codewars.com/kata/55d7e5aa7b619a86ed000070/train/java
+// 2021-12-01 T:21:34:34
+class Exercise_302 {
+
+    public static String orderWord(String s){
+
+        if(s == null || s.length() == 0){
+            return "Invalid String!";
+        }
+
+        return s.chars()
+                .sorted()
+                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                .toString();
+    }
+
+    public static void main(String[] args) {
+        orderWord("!Hi You!");
+    }
+
+
+}
+/*if(s==null||"".equals(s))return "Invalid String!";
+    char [] a = s.toCharArray();
+    java.util.Arrays.sort(a);
+    return String.valueOf(a)
+
+     return (s == null || s.length() == 0) ? "Invalid String!" : s.chars().sorted().mapToObj(c -> String.valueOf((char) c)).collect(Collectors.joining());
+    */
