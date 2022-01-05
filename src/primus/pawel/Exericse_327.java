e5055ca77948a4263cea286269878e6c44e32608
Pawel P <pawelprimus@gmail.com>
2022-01-06 00:20:21 +0100

    Finished exercise 327

--- src/primus/pawel/Exericse_327.java
+++ src/primus/pawel/Exericse_327.java
@@ -1,7 +1,21 @@
 package primus.pawel;
-//
-//
-//
-//
+// Collatz Conjecture Length
+// 7kyu
+// https://www.codewars.com/kata/54fb963d3fe32351f2000102/train/java
+// 2022-01-06 T:00:16:14
 class Exericse_327 {
+
+    public static long conjecture(long x) {
+        int result = 1;
+        long number = x;
+        while (number != 1){
+            if(number%2 == 0 ){
+                number = number/2;
+            } else {
+                number = number*3 + 1;
+            }
+            result++;
+        }
+        return result;
+    }
 }
