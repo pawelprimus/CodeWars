741e53b0d160a13efc33f25179fcfd4c6fa9b20e
Pawel P <pawelprimus@gmail.com>
2022-01-06 01:20:49 +0100

    updated readme

--- src/primus/pawel/Exercise_253_todo.java
+++ src/primus/pawel/Exercise_253_todo.java
@@ -10,44 +10,32 @@ class Exercise_253_todo {
 
     public static Map<Integer, List<String>> removeDuplicateIds(Map<Integer, List<String>> obj) {
 
-        Map<Integer, List<String>> map = new TreeMap<Integer, List<String>>() {};
 
-        List<String> list = null;
-        list.add("aa");
-        map.put(345 , null);
-        ArrayList<Integer> keys = new ArrayList<Integer>(obj.keySet());
-        String currentKey;
-        for(int i=obj.size()-1; i>=0;i--){
-            //System.out.println(obj.get(keys.get(i)));
+        for (Integer key_1 : obj.keySet()) {
 
-            for (int j = 0; j < obj.get(keys.get(i)).size(); j++) {
-                currentKey = obj.get(keys.get(i)).get(j);
+            List<String> loop_1 = obj.get(key_1);
 
-                for (int k = i -2 ; k >= 0 ; k--) {
-                    if (obj.get(keys.get(k)).contains(currentKey)){
-                        //obj.get(keys.get(k)).remove(currentKey);
-                        System.out.println(currentKey);
-                        System.out.println(obj.get(keys.get(k)).remove(0));
-                    }
-                }
-               // System.out.println(obj.get(keys.get(i)).get(j));
-            }
+
+
 
         }
+
 
         return obj;
     }
 
     public static void main(String[] args) {
 
-        Map<Integer, List<String>> map = new TreeMap<Integer, List<String>>() {};
+        Map<Integer, List<String>> map = new TreeMap<Integer, List<String>>() {
+        };
         map.put(432, Arrays.asList(new String[]{"A", "A", "B", "D"}));
-        map.put(53, Arrays.asList(new String[]{"L", "G", "B", "C"}));
+        map.put(53123, Arrays.asList(new String[]{"L", "G", "B", "C"}));
         map.put(236, Arrays.asList(new String[]{"L", "A", "X", "G", "H", "X"}));
         map.put(11, Arrays.asList(new String[]{"P", "R", "S", "D"}));
 
         removeDuplicateIds(map);
 
     }
+
 
 }
---
+++ src/primus/pawel/Exercise_255.java
@@ -0,0 +1,50 @@
+package primus.pawel;
+
+import java.util.*;
+
+import static java.util.Collections.*;
+
+//Difference between two collections
+// 7kyu
+// https://www.codewars.com/kata/594093784aafb857f0000122/train/java
+// 2022-01-06 T:00:31:54
+class Exercise_255 {
+
+    public static List<Character> diff(Collection<Character> a, Collection<Character> b) {
+        List<Character> diffList = new ArrayList<>();
+        for(Character character : a){
+            if(!b.contains(character)){
+                if(!diffList.contains(character)){
+                    diffList.add(character);
+                }
+
+            }
+        }
+        for(Character character : b){
+            if(!a.contains(character)){
+                if(!diffList.contains(character)){
+                    diffList.add(character);
+                }
+            }
+        }
+        Collections.sort(diffList);
+        return diffList;
+    }
+
+    public static void main(String[] args) {
+
+        Collection<Character> col = new ArrayList<>();
+        col.add('a');
+        col.add('b');
+        col.add('z');
+        Collection<Character> col2 = new ArrayList<>();
+        col2.add('a');
+        col2.add('b');
+        List<Character> result = diff(col, col2);
+
+        for(Character charac : result){
+            System.out.println(charac);
+        }
+
+    }
+}
