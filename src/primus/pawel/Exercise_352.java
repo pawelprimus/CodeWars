f8f9b30167b3fb99023da0f89f8e6d78a224fd57
Pawel P <pawelprimus@gmail.com>
2022-01-27 01:05:28 +0100

    Finished exercise 352

--- src/primus/pawel/Exercise_352.java
+++ src/primus/pawel/Exercise_352.java
@@ -1,7 +1,37 @@
 package primus.pawel;
-//
-//
-//
-//
+
+// CubeSummation
+// 7kyu
+// https://www.codewars.com/kata/550e9fd127c656709400024d/train/java
+// 2022-01-27 T:00:58:29
 class Exercise_352 {
+
+    public static long cubeSum(int n, int m) {
+
+        int biggerNum;
+        int smallerNum;
+        long sum = 0;
+        if (n == m) {
+            return 0;
+        }
+
+        smallerNum = Math.min(n, m);
+        biggerNum = Math.max(n, m);
+        sum = java.util.stream.LongStream.range(smallerNum + 1, biggerNum).map(i -> (long) Math.pow(i, 3)).sum();
+
+//        if (n > m) {
+//            biggerNum = n;
+//            smallerNum = m;
+//
+//        } else {
+//            biggerNum = m;
+//            smallerNum = n;
+//        }
+//        for (int i = smallerNum + 1; i <= biggerNum; i++) {
+//
+//            sum += Math.pow(i, 3);
+//
+//        }
+        return sum;
+    }
 }
