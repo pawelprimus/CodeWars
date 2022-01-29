54526829821ce963211d980cf2dad03739a152c2
Pawel P <pawelprimus@gmail.com>
2022-01-29 11:59:15 +0100

    Finished exercise 354

--- src/primus/pawel/Exercise_354.java
+++ src/primus/pawel/Exercise_354.java
@@ -1,7 +1,41 @@
 package primus.pawel;
-//
-//
-// 
-//
+
+import java.util.List;
+
+// Password maker
+// 7kyu
+// https://www.codewars.com/users/%3Cprim%3Erim%3C%2Fprim%3E
+// 2022-01-29 T:11:39:55
 class Exercise_354 {
+
+    public static String makePassword(String phrase) {
+
+        if(phrase.length() == 0){
+            return "";
+        }
+
+        List<String> words = List.of(phrase.split(" "));
+        StringBuilder sb = new StringBuilder();
+        char c;
+
+        for (String word : words) {
+            c = Character.toUpperCase(word.charAt(0));
+            switch (c) {
+                case 'I':
+                    sb.append("1");
+                    break;
+                case 'O':
+                    sb.append("0");
+                    break;
+                case 'S':
+                    sb.append("5");
+                    break;
+                default:
+                    sb.append(word.charAt(0));
+            }
+        }
+
+        return sb.toString();
+    }
+
 }
