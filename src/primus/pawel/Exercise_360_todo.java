ecd758a5e16b15057510a16b847050f273b1250c
Pawel P <pawelprimus@gmail.com>
2022-02-03 23:46:35 +0100

    Exercise 360 todo

---
+++ src/primus/pawel/Exercise_360_todo.java
@@ -0,0 +1,12 @@
+package primus.pawel;
+
+// Four/Seven
+// 7kyu
+// https://www.codewars.com/kata/5ff50f64c0afc50008861bf0/train/java
+// 2022-02-03 T:23:32:34
+class Exercise_360_todo {
+
+    public static int fourSeven(int n) {
+        return null;
+    }
+}
