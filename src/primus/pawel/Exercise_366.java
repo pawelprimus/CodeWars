7873dfaede85631e06c58127154e916034e33361
Pawel P <pawelprimus@gmail.com>
2022-02-10 22:07:29 +0100

    Finished exercise 366

--- src/primus/pawel/Exercise_366.java
+++ src/primus/pawel/Exercise_366.java
@@ -1,7 +1,13 @@
 package primus.pawel;
-//
-//
-//
-//
+
+import java.util.stream.IntStream;
+
+// Series of integers from 0 to n
+// 7kyu
+// https://www.codewars.com/kata/5841f4fb673ea2a2ae000111/train/java
+// 2022-02-10 T:22:02:32
 class Exercise_366 {
+    public static int[] generateIntegers(int n) {
+        return IntStream.rangeClosed(0, n).toArray();
+    }
 }
