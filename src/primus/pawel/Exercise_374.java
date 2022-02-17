fb73cb2c4471c89fdf3e5a95a40cbae65e889a5c
Pawel P <pawelprimus@gmail.com>
2022-02-17 21:22:49 +0100

    Finished exercise 374

---
+++ src/primus/pawel/Exercise_374.java
@@ -0,0 +1,30 @@
+package primus.pawel;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+
+// Aerial Firefighting
+// 7kyu
+// https://www.codewars.com/kata/5d10d53a4b67bb00211ca8af/train/java
+// 2022-02-17 T:21:06:46
+public class Exercise_374 {
+
+    public static int waterbombs(String fire, int w) {
+        String[] fires = fire.split("Y");
+        int result = 0;
+        for (String word : fires) {
+            result += Math.ceil((double) word.length() / w);
+        }
+        return result;
+    }
+
+
+    @Test
+    public void basicTests() {
+        assertEquals(3, Exercise_374.waterbombs("xxxxYxYx", 4));
+        assertEquals(2, Exercise_374.waterbombs("xxYxx", 3));
+    }
+}
