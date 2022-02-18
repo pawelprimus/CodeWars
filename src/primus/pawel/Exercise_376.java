782884702b3518c47a0ea495d133d14b98a1daf4
Pawel P <pawelprimus@gmail.com>
2022-02-18 23:53:41 +0100

    Finished Exercise 376

--- src/primus/pawel/Exercise_376.java
+++ src/primus/pawel/Exercise_376.java
@@ -1,14 +1,19 @@
 package primus.pawel;
+
 // Automorphic Number (Special Numbers Series #6)
 // 7kyu
 // https://www.codewars.com/kata/5a58d889880385c2f40000aa/train/java
 // 2022-02-18 T:23:48:34
 class Exercise_376 {
 
+    private static final String CORRECT = "Automorphic";
+    private static final String NOT = "Not!!";
 
+    public static String autoMorphic(int number) {
 
-    public static String autoMorphic(int number)
-    {
-        return "Do your magic!";
+        String notSquared = String.valueOf(number);
+        String squared = String.valueOf(number * number);
+
+        return squared.substring(squared.length() - notSquared.length()).equals(notSquared) ? CORRECT : NOT;
     }
 }
