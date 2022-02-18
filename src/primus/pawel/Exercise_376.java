f8b693650c24775a3dc174ed97ebacff212f9ede
Pawel P <pawelprimus@gmail.com>
2022-02-18 23:49:17 +0100

    Exercise 376

---
+++ src/primus/pawel/Exercise_376.java
@@ -0,0 +1,14 @@
+package primus.pawel;
+// Automorphic Number (Special Numbers Series #6)
+// 7kyu
+// https://www.codewars.com/kata/5a58d889880385c2f40000aa/train/java
+// 2022-02-18 T:23:48:34
+class Exercise_376 {
+
+
+
+    public static String autoMorphic(int number)
+    {
+        return "Do your magic!";
+    }
+}
