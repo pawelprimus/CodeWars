56bdabf59194dbe699c89792343dca14fc25e8a3
Pawel P <pawelprimus@gmail.com>
2022-03-10 22:07:57 +0100

    Finished exercise 394

---
+++ src/primus/pawel/Exercise_394.java
@@ -0,0 +1,29 @@
+package primus.pawel;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+// Letterbox Paint-Squad
+// 7kyu
+// https://www.codewars.com/kata/597d75744f4190857a00008d/train/java
+// 2022-03-10 T:21:55:59
+class Exercise_394 {
+
+    public static int[] paintLetterboxes(final int start, final int end) {
+        int[] result = new int[10];
+
+        IntStream.rangeClosed(start, end).forEach(i ->{
+            String strNum = String.valueOf(i);
+            for (int j = 0; j < strNum.length(); j++) {
+                result[strNum.charAt(j) - 48]++;
+            }
+        });
+        return result;
+    }
+
+    public static void main(String[] args) {
+        paintLetterboxes(10, 77);
+    }
+
+
+}
