a980cb66bbfc9757b6249077de83fb46306b1c54
Pawel P <pawelprimus@gmail.com>
2022-03-12 13:22:09 +0100

    Updated readme

--- src/primus/pawel/StatsMd.java
+++ src/primus/pawel/StatsMd.java
@@ -42,7 +42,7 @@ public class StatsMd {
     }
 
     private void setStats(String languateType) {
-        stats.append("languateType");
+        stats.append(BR_END).append(languateType).append(BR_END);
         stats.append(getKyuStats(kyulevels));
         stats.append(("<br />") + ("<br />") + ("DONE: " + done) + ("<br />"));
         stats.append("TODO: " + todo + "<br />").append(BR_END);
