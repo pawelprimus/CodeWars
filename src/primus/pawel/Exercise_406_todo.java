02032e1530793ef2b24a6a8482d95712d982fe39
Pawel P <pawelprimus@gmail.com>
2022-03-29 23:47:08 +0200

    Exercise 409 todo

--- src/primus/pawel/Exercise_406_todo.java
+++ src/primus/pawel/Exercise_406_todo.java
@@ -1,4 +1,32 @@
 package primus.pawel;
 
+// Possibilities Array
+// 7kyu
+// https://www.codewars.com/kata/59b710ed70a3b7dd8f000027/train/java
+// 2022-03-29 T:00:56:40
 class Exercise_406_todo {
+
+
+    public static boolean isAllPossibilities(int[] arg) {
+        boolean[] flags = new boolean[arg.length];
+
+
+        for (int i = 0; i < arg.length; i++) {
+            if (arg[i] >= arg.length || arg[i] < 0) {
+                return false;
+            } else {
+                flags[arg[i]] = true;
+            }
+        }
+
+        for (boolean flag : flags) {
+            if (!flag) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+
 }
---
+++ src/primus/pawel/Exercise_409_todo.java
@@ -0,0 +1,4 @@
+package primus.pawel;
+
+class Exercise_409_todo {
+}
