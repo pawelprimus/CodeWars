49988cc7066150c4ac687e0e933a1be246f24091
Pawel P <pawelprimus@gmail.com>
2022-04-26 20:06:58 +0200

    Finished exercise 434

--- src/primus/pawel/Exercise_434.java
+++ src/primus/pawel/Exercise_434.java
@@ -1,13 +1,46 @@
 package primus.pawel;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
 // Where is my parent!?(cry)
 // 6kyu
 // https://www.codewars.com/kata/58539230879867a8cd00011c/train/java
-// 2022-04-25 T:23:26:28
+// 2022-04-26 T:19:32:31
 class Exercise_434 {
 
     static String findChildren(final String text) {
-        return "";
+
+        Map<Character, String> charGroups = new TreeMap<>();
+
+        for (Character c : text.toCharArray()) {
+            Character upperCase = Character.toUpperCase(c);
+            if (charGroups.containsKey(upperCase)) {
+                charGroups.put(upperCase, charGroups.get(upperCase) + c);
+            } else {
+                charGroups.put(upperCase, String.valueOf(c));
+            }
+        }
+
+        return new ArrayList<>(charGroups.values()).stream().map(i -> sortString(i)).collect(Collectors.joining());
     }
 
+    public static String sortString(String inputString) {
+        char tempArray[] = inputString.toCharArray();
+        Arrays.sort(tempArray);
+        return new String(tempArray);
+    }
+
+    public static void main(String[] args) {
+        findChildren("CbcBcbaA");
+    }
 
 }
+
+/* static String findChildren(final String text) {
+        String[] sorted = text.split("");
+        Arrays.sort(sorted);
+        Arrays.sort(sorted, String.CASE_INSENSITIVE_ORDER);
+        return String.join("", sorted);
+    }
+    */
