e1462a11c1a5482e8c6b14fe8de369e2b3d73970
Pawel P <pawelprimus@gmail.com>
2022-05-01 19:33:48 +0200

    Exercise 440 todo

---
+++ src/JS/Exercise_041.js
@@ -0,0 +1,10 @@
+//
+//
+//
+//
+// 2022-04-30 T:11:58:47
+
+function abbrevName(name){
+    var splited =  name.split(" ");
+    return splited[0].charAt(0).toUpperCase() + "." + splited[1].charAt(0).toUpperCase();
+}
