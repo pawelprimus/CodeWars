25856f9bd515bd040d6419e5b0552a4989b38e0a
Pawel P <pawelprimus@gmail.com>
2022-05-16 21:16:14 +0200

    Finished Exercise_450

--- src/primus/pawel/Exercise_450_todo.java
+++ src/primus/pawel/Exercise_450_todo.java
@@ -1,4 +1,18 @@
 package primus.pawel;
 
+import java.time.LocalDate;
+
+// Count the number of days between two dates
+// 6kyu
+// https://www.codewars.com/kata/59569ba543e2a8ebda00012a/train/java
+// 2022-05-16 T:21:09:16
 class Exercise_450_todo {
+
+    public static long getDaysAlive(int year, int month, int day, int year2, int month2, int day2) {
+
+        LocalDate ld1 = LocalDate.of(year, month, day);
+        LocalDate ld2 = LocalDate.of(year2, month2, day2);
+
+        return ld1.datesUntil(ld2).count();
+    }
 }
