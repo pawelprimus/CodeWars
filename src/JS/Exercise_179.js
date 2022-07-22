fe40dc8f036630a33c5ee212c9ed2da8cb3e0e8a
Pawel P <pawelprimus@gmail.com>
2022-07-22 08:53:31 +0200

    Exercises 178_189 todo

---
+++ src/JS/Exercise_179.js
@@ -0,0 +1,9 @@
+// Counting Valleys
+// 7kyu
+// https://www.codewars.com/kata/5da9973d06119a000e604cb6/train/javascript
+// 2022-07-22 T:08:47:41
+
+function countingValleys(s) {
+}
+
+console.log(countingValleys('FUFFDDFDUDFUFUF'))
