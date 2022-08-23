7e8f6a831a1a8b1a00097846c2533a4ca71df7c0
Pawel Primus <pawel.primus@snte.pl>
2022-08-23 21:13:25 +0200

    Finished exercise 469

---
+++ src/primus/pawel/Exercise_469.java
@@ -0,0 +1,31 @@
+package src.primus.pawel;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.stream.Collectors;
+
+// Suzuki needs help lining up his students!
+// 7kyu
+// https://www.codewars.com/kata/5701800886306a876a001031/train/java
+// 2022-08-23 T:17:14:04
+public class Exercise_469 {
+
+  public static String[] lineupStudents(String students)
+  {
+
+    return Arrays.asList(students.split(" ")).stream()
+        .sorted(Exercise_469::compare)
+        .toArray(String[]::new);
+  }
+
+  public static int compare(String o1, String o2) {
+    if(o1.length() == o2.length()){
+      return o2.compareTo(o1);
+    }
+    return o1.length() > o2.length() ? -1 : 1;
+  }
+
+  public static void main(String[] args) {
+    lineupStudents("Tadashi Takahiro Takao Takashi Takayuki Takehiko Takeo Takeshi Takeshi");
+  }
+}
