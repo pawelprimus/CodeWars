b9caff3d98ade09b59a5c99da5156530c99986f9
pawelprimus <pawelprimus@gmail.com>
2022-09-04 20:38:59 +0200

    Exercise 477 todo

---
+++ src/primus/pawel/Exercise_477_todo.java
@@ -0,0 +1,25 @@
+package src.primus.pawel;
+
+// Closest to Zero
+// 7kyu
+// https://www.codewars.com/kata/59887207635904314100007b/train/java
+// 2022-09-04 20:23
+public class Exercise_477_todo {
+
+  public static Integer find(int[] arr) {
+
+
+
+
+
+  }
+
+  public static void main(String[] args) {
+
+    find(new int[]{10, 3, 9, 1});
+    find(new int[]{2, 4, -1, -3});
+    find(new int[]{5, 1, -1, 2, -10});
+  }
+
+
+}
