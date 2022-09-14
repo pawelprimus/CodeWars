7a34c96b311c21f38e5361ead8c5f3d60cdb7fa8
pawelprimus <pawelprimus@gmail.com>
2022-09-14 17:30:44 +0200

    Finished Exercise 483

---
+++ src/primus/pawel/Exercise_483.java
@@ -0,0 +1,11 @@
+package src.primus.pawel;
+// Grasshopper - Messi goals function
+// 8kyu
+// https://www.codewars.com/kata/55f73be6e12baaa5900000d4/train/java
+// 2022-09-14 T:17:22:29
+public class Exercise_483 {
+
+  public static int goals(int laLigaGoals, int copaDelReyGoals, int championsLeagueGoals) {
+    return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+  }
+}
--- src/primus/pawel/Exercise_483_todo.java
+++
@@ -1,4 +0,0 @@
-package src.primus.pawel;
-
-public class Exercise_483_todo {
-}
